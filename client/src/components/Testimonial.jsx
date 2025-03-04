a202295e279c5e8d84a01a572c8e77cbb8da2f66
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-03-04 17:20:01 +0530

    1 day commit

---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { assets, testimonialsData } from '../assets/assets'
+
+const Testimonial = () => {
+  return (
+    <div className='flex flex-col items-center jusity-center my-20 py-12 md:px-28'>
+      <h1 className='text-3xl sm:text-4xl font'>Customer Testimonials</h1>
+      <p className='text-gray-500 mb-12'>What Out Users Are Saying</p>
+
+
+      <div className='flex flex-wrap gap-6'>
+        {
+            testimonialsData.map((item,i)=>(
+                <div key={i} className='bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all'>
+                    <div className='flex flex-col items-center'>
+                        <img className='rounded-full w-14' src={item.image} alt="" />
+                        <h2 className='text-xl font-semibold mt-3'>{item.name}</h2>
+                        <p className='text-gray-500 mb-4'>{item.role}</p>
+                        <div className='flex mb-4'>
+                            {Array(item.stars).fill().map((item,i)=>(
+                                <img src={assets.rating_star} key={i}/>
+                            ))}
+                        </div>
+                        <p className='text-center text-sm text-gray-600'>{item.text}</p>
+                    </div>
+
+                </div>
+            ))
+        }
+      </div>
+
+    </div>
+  )
+}
+
+export default Testimonial
